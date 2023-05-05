efbf8e6e04a90259b251a0754e657b7a82c86e3d
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-05-05 22:52:27 +0100

    Create qrgc.sh

---
+++ src/qrgc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /opt/QR-Code-Generator
+
+python3 -m venv venv
+source venv/bin/activate
+
+# install requirements in venv
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pyqrcode
+
+# run the script
+python3 /opt/QR-Code-Generator/src/main.py
+
+sleep 1m
+
+deactivate
+
+rm -r venv
