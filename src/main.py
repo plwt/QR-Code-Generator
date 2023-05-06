977fe1778745a090ffc5895b17d8971c7b5d3c26
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-05-06 21:00:48 +0100

    add exit

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ def qrcg():
     address = input("Enter URL: ")
     qr_code = pyqrcode.create(address)
     qr_code.png("image.png", scale=5)
-    Image.open("image.png")
+    img = Image.open("/QR-Code-Generator/image.png")
+    img.show()
+    exit()
 
 qrcg()
